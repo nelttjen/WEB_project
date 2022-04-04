c2d7d5ac24f7b2d3b1674a822d56354497f1d37d
Artemy <tatyapa0880@yandex.ru>
2022-04-05 01:18:00 +0500

    footer + header

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from App.Config import Config
 from App.settings import *
 
@@ -9,6 +9,15 @@ config = Config(app)
 @app.route('/')
 def index():
     return render_template("base.html", css=url_for('static', filename='css/index.css'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'login'
+
+    return render_template("login.html", title='Авторизация', css=url_for('static', filename='css/login.css'))
+
 
 
 if __name__ == '__main__':
