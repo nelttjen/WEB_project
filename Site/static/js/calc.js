2b536552ea82ec3a5229c34afc35cb991ff881a5
Artemy <tatyapa0880@yandex.ru>
2022-04-29 01:44:32 +0500

    calc work (ya spat)

---
+++ Site/static/js/calc.js
@@ -0,0 +1,26 @@
+$.event.special.inputchange = {
+        setup: function() {
+            var self = this, val;
+            $.data(this, 'timer', window.setInterval(function() {
+                val = self.value;
+                if ( $.data( self, 'cache') != val ) {
+                    $.data( self, 'cache', val );
+                    $( self ).trigger( 'inputchange' );
+                }
+            }, 20));
+        },
+        teardown: function() {
+            window.clearInterval( $.data(this, 'timer') );
+        },
+        add: function() {
+            $.data(this, 'cache', this.value);
+        }
+    };
+
+$('#from').on("inputchange", function () {
+    $('#from-input').val($('#from').val());
+});
+
+$('#to').on("inputchange", function () {
+    $('#to-input').val($('#to').val());
+});
